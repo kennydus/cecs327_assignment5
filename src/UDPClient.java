b0e888477203fc86aada5eedd73e74b6680ef363
Kenny Du <61264046+kennydus@users.noreply.github.com>
2022-11-09 12:48:16 -0800

    Finished UDP Server. TODO: Client sends a blank message when initially entering the while loop.

--- src/UDPClient.java
+++ src/UDPClient.java
@@ -7,10 +7,10 @@ public class UDPClient{
         DatagramSocket aSocket = null;
 
         Scanner in = new Scanner(System.in);        // Scanner object for user input
-        System.out.println("Please enter the IP Adresss of the server: ");
-        String ipAddr = in.nextLine();
+        System.out.println("Please enter the IP Address of the server: ");
+        String ipAddr = in.nextLine();              // Gets IP address from input
         System.out.println("Please enter the Port Number of the server: ");
-        int portNum = in.nextInt();
+        int portNum = in.nextInt();                 // Gets port number from input
 
         // args give message contents and server hostname
         try {
@@ -20,16 +20,16 @@ public class UDPClient{
                 System.out.println("Enter a message to send to the server: ");
                 String message = in.nextLine();
 
-
                 byte[] m = message.getBytes();
-                InetAddress aHost = InetAddress.getByName(ipAddr);
-                int serverPort = portNum;
-                DatagramPacket request = new DatagramPacket(m, m.length, aHost, serverPort);
+                InetAddress aHost = InetAddress.getByName(ipAddr);      // Creates IP address from inputted IP string
+
+                DatagramPacket request = new DatagramPacket(m, m.length, aHost, portNum);   // Creates packet with
+                                                                                        // contents of the new message
                 aSocket.send(request);
                 byte[] buffer = new byte[1000];
                 DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
                 aSocket.receive(reply);
-                System.out.println("Reply: " + new String(reply.getData()));
+                System.out.println("Reply: " + new String(reply.getData()) + "\n");
             }
         }
         catch (SocketException e){
--- src/UDPServer.java
+++ src/UDPServer.java
@@ -1,17 +1,33 @@
 import java.net.*;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class UDPServer{
     public static void main(String args[]){
         DatagramSocket aSocket = null;
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter a Port Number for the server: ");
+        int portNum = in.nextInt();
+
         try{
-            aSocket = new DatagramSocket(6789);
-            byte[] buffer = new byte[1000];
+            aSocket = new DatagramSocket(portNum);
             while(true) {
+                byte[] buffer = new byte[1000];
                 DatagramPacket request = new DatagramPacket(buffer, buffer.length);
                 aSocket.receive(request);
-                DatagramPacket reply = new DatagramPacket(request.getData(),
-                        request.getLength(), request.getAddress(), request.getPort());
-                aSocket.send(reply);
+                String receivedMessage = new String(request.getData());    // takes client received message and converts
+                                                                           // to string
+
+                System.out.println("Client Message: " + receivedMessage);
+
+                buffer = receivedMessage.toUpperCase().getBytes();      // Puts the client message in Upper Case form
+                                                                        // and stores it into the buffer
+
+                DatagramPacket reply = new DatagramPacket(buffer, buffer.length,    // Creating reply packet
+                        request.getAddress(), request.getPort());
+                aSocket.send(reply);                                    // Sending reply packet to the client
             }
         }
         catch (SocketException e) {
@@ -21,7 +37,8 @@ public class UDPServer{
             System.out.println("IO: " + e.getMessage());
         }
         finally {
-            if(aSocket != null) aSocket.close();
+            if(aSocket != null)
+                aSocket.close();
         }
     }
 }
